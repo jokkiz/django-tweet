c0f02e74573920b3c5f516af347d9a10d246fc3d
Ruslan <r.dulkarnaev@mail.ru>
2016-03-20 15:51:55 +0300

    first add

---
+++ tweets/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.generic import View
+# Create your views here.
+class Index(View):
+    def get(self, request):
+        params = {}
+        params["name"] = "Django"
+        return render(request, 'base.html', params)
+    def post(self, request):
+        return HttpResponse("Запрос отправлен")
